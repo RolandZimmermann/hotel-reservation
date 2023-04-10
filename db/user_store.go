8e0593c29b87e4fdcad1c9b44afdd51a7fad25b4
RolandZimmermann <roland.zimmermann0@gmail.com>
2023-04-10 21:33:32 +0200

    better user api

--- db/user_store.go
+++ db/user_store.go
@@ -13,11 +13,12 @@ const userColl = "users"
 
 type UserStore interface {
 	GetUserByID(context.Context, string) (*types.User, error)
+	GetUsers(context.Context) ([]*types.User, error)
+	InsertUser(context.Context, *types.User) (*types.User, error)
 }
 
 type MongoUserStore struct {
 	client *mongo.Client
-	dbname string
 	coll   *mongo.Collection
 }
 
@@ -26,6 +27,15 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 		client: client,
 		coll:   client.Database(DBNAME).Collection(userColl),
 	}
+}
+
+func (s *MongoUserStore) InsertUser(ctx context.Context, user *types.User) (*types.User, error) {
+	res, err := s.coll.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	return user, nil
 }
 
 func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
@@ -39,4 +49,17 @@ func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.Use
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
+	curr, err := s.coll.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	var users []*types.User
+	if err := curr.All(ctx, &users); err != nil {
+		return nil, err
+	}
+	return users, nil
 }
